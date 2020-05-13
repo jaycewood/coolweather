b9a289107a47b6e09b8c8b08f805bce70a93cf0c
jaycewood <jaycewood1024@gmial.com>
2020-05-13 20:59:01 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,42 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private int cityCode;
+    private int provinceId;
+    private String cityName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String  cityName) {
+        this.cityName = cityName;
+    }
+}
